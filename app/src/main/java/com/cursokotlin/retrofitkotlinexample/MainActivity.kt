d8452ae0fdb5e0c57dca0e91491b095c39f12af7
Aris <aristides.guimera@mo2o.com>
2018-04-29 16:18:36 +0200

    feat: prepare retrofit call

--- app/src/main/java/com/cursokotlin/retrofitkotlinexample/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/retrofitkotlinexample/MainActivity.kt
@@ -2,7 +2,12 @@ package com.cursokotlin.retrofitkotlinexample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(), android.support.v7.widget.SearchView.OnQueryTextListener {
 
@@ -11,6 +16,33 @@ class MainActivity : AppCompatActivity(), android.support.v7.widget.SearchView.O
         setContentView(R.layout.activity_main)
         searchCharapter.queryHint = "Ej: Hodor"
         searchCharapter.setOnQueryTextListener(this)
+        doAsync {
+            var call = getRetrofit().create(APIService::class.java).getCharacterByName("characters/hodor").execute()
+            var character = call.body() as ModelResponse
+            uiThread {
+                initCharacter(character)
+            }
+
+
+
+
+        }
+
+    }
+
+    private fun initCharacter(character: ModelResponse) {
+
+    }
+
+    fun toast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl("https://api.got.show/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
     }
 
 
