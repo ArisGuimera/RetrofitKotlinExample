35552f9b86222d62fbb72507953fa54d3f6e90ce
Aris <aristides.guimera@mo2o.com>
2018-04-29 16:17:14 +0200

    feat: created APIService

---
+++ app/src/main/java/com/cursokotlin/retrofitkotlinexample/APIService.kt
@@ -0,0 +1,13 @@
+package com.cursokotlin.retrofitkotlinexample
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+/**
+ * Created by aristidesguimeraorozco on 29/4/18.
+ */
+interface APIService {
+    @GET()
+    fun getCharacterByName(@Url url:String): Call<ModelResponse>
+}
